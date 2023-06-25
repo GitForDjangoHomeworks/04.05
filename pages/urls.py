9f496ace5e306d58450b6d1a7f4a3e036123a1db
Diyor <diyor.iknu@gmail.com>
2023-06-25 19:16:14 +0500

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import(
+
+    AllProductsView, 
+    add_file,
+    get_file,
+    index_files
+)
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('all_products', AllProductsView.as_view(), name='all_products'),
+    path('add_file',add_file,name = 'add_file'),
+    path('files/get/<path:filename>', get_file, name = 'get_file'),
+    path('files/',index_files, name='index_files'),
+
+ ]
